16a87cc44d435c9f7ee5cb8219a97e8d62eac669
alexgimenez02 <alex.gimenez.saludes@gmail.com>
2025-07-29 00:48:52 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,5 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import "leaflet/dist/leaflet.css"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
